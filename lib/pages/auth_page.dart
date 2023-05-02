98fb092aba66a3864ede760c7cb4a4eeffd0c831
Brandon <brandonvqz23gmail.com>
2023-05-02 13:04:18 -0600

    integracion con firebase

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
  
 
 class AuthPage extends StatelessWidget {
-  const AuthPage({super.key})
+   AuthPage({super.key})
 
 @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold(body: StreamBuilder);
   }
 }
